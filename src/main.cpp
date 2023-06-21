2dc05857539aa8b5d69a00240aec3c67fabb47d0
Joseph Boyer <jboyer@uvic.ca>
2023-06-21 15:33:03 -0700

    Commented out wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <WiFi.h>
 #include <SPIFFS.h>
 #include <ESPAsyncWebServer.h>
-#include <OneWire.h>
+#include <DS18B20.h>
 
 AsyncWebServer server(80);
-const char* SSID = "spca";
-const char* PASSWORD = "spca1234";
+const char* SSID = "Peachy2.4";
+const char* PASSWORD = "Friendly";
+OneWire glycol(16);
+DS18B20 glycolSen(&glycol);
 
 
 
@@ -16,21 +18,25 @@ void setup() {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  WiFi.begin(SSID, PASSWORD);
-  while(WiFi.status() != WL_CONNECTED){
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.printf("Connected to the WiFi network with IP Address: %s\n", WiFi.localIP().toString().c_str());
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html");
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", "50");
-  });
-  server.begin();
+  // WiFi.begin(SSID, PASSWORD);
+  // while(WiFi.status() != WL_CONNECTED){
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi..");
+  // }
+  // Serial.printf("Connected to the WiFi network with IP Address: %s\n", WiFi.localIP().toString().c_str());
+  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  //   request->send(SPIFFS, "/index.html");
+  // });
+  // server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+  //   request->send_P(200, "text/plain", "50");
+  // });
+  // server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Regquesting Sensor Temperature");
+  glycolSen.requestTemperatures();
+  while(!glycolSen.isConversionComplete());
+  Serial.printf("Sensor Temperature: %f\n", glycolSen.getTempC());
+  delay(5000);
 }
